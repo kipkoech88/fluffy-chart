64b938060d09a4622f2e595a105a5bd44fa86169
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-28 16:01:47 +0300

    project set up

---
+++ main.js
@@ -0,0 +1,22 @@
+const myChart = document.getElementsById("myChart")
+import Chart from "chart.js/auto"
+
+
+new Chart(myChart, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
